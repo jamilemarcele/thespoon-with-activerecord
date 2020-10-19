ebc152a98b8cab77135f8f83a572e449e31eb6c2
Jamile Radloff <jamilemarcele@gmail.com>
2020-10-19 15:47:24 +0200

    view files and modifications in the controller, no css

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   # CREATE, READ, UPDATE, DELETE
   # verb 'path', to: 'controller#action'
   # read all restaurants
-  # get '/restaurants', to: 'restaurants#index'
-  # # read one restarurant
-  # get '/restaurants/:id', to: 'restaurants#show'
-  # # create a restaurant
-  # get '/restaurants/new', to: 'restaurants#new'
-  # post '/restaurants', to: 'restaurants#create'
-  # # update a restaurant
-  # get '/restaurants/:id/edit', to: 'restaurants#edit'
-  # patch '/restaurants/:id', to: 'restaurants#update'
-  # # delete a restaurant
-  # delete 'restaurants/:id', to: 'restaurants#destroy'
-  resources :restaurants
+  get '/restaurants', to: 'restaurants#index'
+  # read one restaurant
+  get '/restaurants/:id', to: 'restaurants#show'
+  # create a restaurant
+  get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
+  # update a restaurant
+  get '/restaurants/:id/edit', to: 'restaurants#edit'
+  patch '/restaurants/:id', to: 'restaurants#update'
+  # delete a restaurant
+  delete 'restaurants/:id', to: 'restaurants#destroy'
+  # resources :restaurants
 end
